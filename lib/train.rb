8d960bd7100fa0836cf9de5375ee109615e3411b
1278dale <dalestevens1278@gmail.com>
2014-10-31 12:45:55 +0000

    Added lib and spec directories

---
+++ lib/train.rb
@@ -0,0 +1,2 @@
+def Train
+end
---
+++ spec/train_spec.rb
@@ -0,0 +1,24 @@
+require 'train'
+
+describe Train do
+  
+  it "should be able to" do
+  end
+end
+
+
+
+
+
+# # Building a system that manages trains that can be loaded by users at a station,
+# and then depart with users once loaded. There are a certain number of
+# trains in the system. 
+# Trains travel from station to station. 
+# When in a station, passengers can enter and alight the trains. 
+# Once the capacity limit has been reached, no more passengers can
+# enter the train.  
+# Passengers touch in at station, and touch out of destination station.
+# Each coach of a train can hold up to 40 passengers
+# Each train is made up of 10 coaches
+# If the capacity limit of a carriage or train is reached,
+# no passengers will be able to enter.
