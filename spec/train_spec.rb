a1d3ae3aa2df59facabe580d9ab24d0f3cc70fb5
1278dale <dalestevens1278@gmail.com>
2014-10-31 14:56:44 +0000

    Added spec and class files for our objects in faast system

--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -2,7 +2,8 @@ require 'train'
 
 describe Train do
   
-  it "should be able to" do
+  it "should be able to accept passengers" do
+    expect
   end
 end
 
@@ -10,15 +11,13 @@ end
 
 
 
-# # Building a system that manages trains that can be loaded by users at a station,
-# and then depart with users once loaded. There are a certain number of
-# trains in the system. 
+# Building a system that manages trains that can be loaded by users at a station,
+# and then depart with users once loaded. There are a certain number of trains in the system. 
 # Trains travel from station to station. 
 # When in a station, passengers can enter and alight the trains. 
-# Once the capacity limit has been reached, no more passengers can
-# enter the train.  
+# Once the capacity limit has been reached, no more passengers can enter the train.  
 # Passengers touch in at station, and touch out of destination station.
 # Each coach of a train can hold up to 40 passengers
-# Each train is made up of 10 coaches
+# Each train is made up of a number(eg.10?) coaches
 # If the capacity limit of a carriage or train is reached,
 # no passengers will be able to enter.
