4727ab2044d66aee8a2380dede61f0e8b0ef8a2b
1278dale <dalestevens1278@gmail.com>
2014-10-31 15:33:00 +0000

    Added passenger class and spec file

---
+++ lib/passengers.rb
@@ -0,0 +1,17 @@
+class Passengers
+  def enter?
+    true
+  end
+
+  def alight?
+    true
+  end
+
+  def touchin?
+    true
+  end
+
+  def touchout?
+    true
+  end
+end
---
+++ spec/passengers_spec.rb
@@ -0,0 +1,29 @@
+# should be able to enter coach
+# should be able to alight coach
+# should be able to touch into station
+# should be able to touch out of station
+require 'passengers'
+
+
+describe Passengers do
+
+  it "should be able to enter coach" do
+    passengers = Passengers.new
+    expect(passengers).to be_enter
+  end
+
+  it "should be able to alight coach" do
+    passengers = Passengers.new
+    expect(passengers).to be_alight
+  end
+
+  it "should be able to touch into station" do
+    passengers = Passengers.new
+    expect(passengers).to be_touchin
+  end
+
+  it "should be able to touch out of station" do
+    passengers = Passengers.new
+    expect(passengers).to be_touchout
+  end
+end
