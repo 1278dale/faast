ea4ba33503ccf30915eebb30385f976301806b84
1278dale <dalestevens1278@gmail.com>
2014-11-03 11:52:23 +0000

    Train spec not finished

---
+++ spec/train_spec.rb
@@ -0,0 +1,7 @@
+# should be able to receive passengers
+# it should be able to alight passengers
+# should know how many passengers are currently on
+
+it "should be able to receive passengers" do
+
+end
